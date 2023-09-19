92b43e45d1db29673224d0cf8f3c5c8b2dbcd143
박영준 <qkrdudwns98@soongsil.ac.kr>
2023-09-19 17:27:18 +0900

    1:1 Client-Server

--- client/client.cpp
+++ client/client.cpp
@@ -2,56 +2,44 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
-#include <thread>
 
 using namespace std;
 
-void ReceiveFromServer(const int server_socket);
 void InitClient(int& client_socket, struct sockaddr_in& address);
 
 int main(int argc, char const* argv[]){
-    
     int client_socket;
     struct sockaddr_in address;
     InitClient(client_socket, address);
 
-    // receiver thread
-    thread receiver(ReceiveFromServer, client_socket);
-    receiver.detach();
+    // client는 먼저 받는다.
+    char recv_buffer[1024] = {0};
+    recv(client_socket, recv_buffer, sizeof(recv_buffer), 0);
+    cout << "\n받은 메시지: " << recv_buffer << endl;
 
-    // Send Message
-    while(true){
-        char buffer[1024] = {0};
-        cin >> buffer;
-        send(client_socket, buffer, sizeof(buffer), 0);
-    }
+    // 메세지를 보낸다.
+    char send_buffer[1024] = {0};
+    cin >> send_buffer;
+    send(client_socket, send_buffer, sizeof(send_buffer), 0);
 
 }
 
 void InitClient(int& client_socket, struct sockaddr_in& address){
-
     const int kPORT = 8080;
     const char* server_address = "127.0.0.1";
 
     // Create Socket file descriptor
-    assert((client_socket= socket(AF_INET, SOCK_STREAM, 0)) >= 0);
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    assert(client_socket >= 0);
 
     address.sin_family = AF_INET;
     address.sin_port = htons(kPORT);
 
     // Convert IPv4 and IPv6 ddresses from text to binary form
-    assert(inet_pton(AF_INET, server_address, &address.sin_addr) > 0);
+    int inet_pton_success = inet_pton(AF_INET, server_address, &address.sin_addr);
+    assert(inet_pton_success > 0);
 
     // Connect to the server
-    assert(connect(client_socket, (struct sockaddr* )&address, sizeof(address)) >= 0);
-
-}
-
-void ReceiveFromServer(const int client_socket){
-    while(1){
-        char buffer[1024] = {0};
-        recv(client_socket, buffer, 1024, 0);
-        cout << "받음: " << buffer << endl;
-    }
+    int connect_success = connect(client_socket, (struct sockaddr* )&address, sizeof(address));
+    assert(connect_success >= 0);
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <cstring>
-#include <thread>
-#include <string>
 #include <cassert>
-#include <algorithm>
-#include <shared_mutex>
 
 using namespace std;
 
-void BroadCastToClient(const string msg);
-void ReceiveFromClient(const int client_socket);
 void InitServer(int& server_socket, struct sockaddr_in& address);
-void RemoveClient(const int client_socket);
-
-vector<int> clients;
-shared_mutex clients_mutex;
 
 int main(){
     int server_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
+    // 서버 소켓 설정
     InitServer(server_socket, address);
 
-    // Accept incoming connection
-    while(1){
-        int client_socket = accept(server_socket, (struct sockaddr* )&address, (socklen_t* )&addrlen);
+    // 클라이언트 연결 대기
+    int client_socket = accept(server_socket, (struct sockaddr* )&address, (socklen_t* )&addrlen);
+    assert(client_socket >= 0);
 
-        assert(client_socket >= 0);
+    // 보낼 메시지 입력 후 전송
+    char send_buffer[1024] = {0};
+    cin >> send_buffer;
+    send(client_socket, send_buffer, sizeof(send_buffer), 0);
 
-        clients.push_back(client_socket); // 연결 대상에 추가
-
-        thread receiver(ReceiveFromClient, client_socket); // 수신할 스레드 추가
-        receiver.detach(); 
-    }
+    // 메시지를 수신할 때까지 대기, 수신 후 출력, 종료
+    char recv_buffer[1024] = {0};
+    recv(client_socket, recv_buffer, sizeof(recv_buffer), 0);
+    cout << "\n받은 메시지: " << recv_buffer << endl;
 
     return 0;
 }
@@ -44,56 +35,20 @@ int main(){
 void InitServer(int& server_socket, struct sockaddr_in& address){
     const int kPORT = 8080;
     int socket_opt = 1;
-    // Creaating socket file descriptor
-    assert((server_socket = socket(AF_INET, SOCK_STREAM, 0)) != 0);
 
-    // Attaching socket to the port 8080
-    assert(!setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_opt, sizeof(socket_opt)));
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    assert(server_socket != 0);
+
+    int setsockopt_success = setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_opt, sizeof(socket_opt));
+    assert(!setsockopt_success);
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(kPORT);
 
-    // Bind the socket to prot
-    assert(::bind(server_socket, (struct sockaddr* )&address, sizeof(address)) >= 0);
+    int bind_success = ::bind(server_socket, (struct sockaddr* )&address, sizeof(address));
+    assert(bind_success >= 0);
 
-    // listen incomming connection
-    assert(listen(server_socket, 3) >= 0);
-}
-
-void BroadCastToClient(const string msg){
-    shared_lock<shared_mutex> lock(clients_mutex);
-    for(int client: clients){
-        send(client, msg.c_str(), msg.size(), 0);
-    }
-}
-
-void ReceiveFromClient(const int client_socket){
-    try {
-        while (true) {
-            char buffer[1024] = {0};
-            int read_byte = recv(client_socket, buffer, sizeof(buffer), 0);
-            assert(read_byte >= 0);
-
-            // 해당 Client의 접속 종료
-            if(read_byte == 0){
-                RemoveClient(client_socket);
-
-                string out_msg = to_string(client_socket) + "님이 퇴장하였습니다.";
-                BroadCastToClient(out_msg);
-                break;
-            }
-
-            BroadCastToClient(string(buffer));
-        }
-    }
-    catch (const std::exception& e) {
-        cerr << "ReceiveFromClient: " << e.what() << endl;
-    }
-}
-
-void RemoveClient(const int client_socket){
-    // 클라이언트 삭제
-    unique_lock<shared_mutex> lock(clients_mutex);
-    clients.erase(remove(clients.begin(), clients.end(), client_socket), clients.end());
+    int listen_success = listen(server_socket, 3);
+    assert(listen_success >= 0);
 }
